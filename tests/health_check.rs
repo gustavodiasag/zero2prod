f479993ff84ab87d29bd940dc19e84caecd2273d
Gustavo Dias <gustavodias.aguiar1@gmail.com>
2024-03-12 19:26:41 -0300

    test: add suite for valid and invalid subscription requests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -22,3 +22,50 @@ mod health_check_endpoint {
         response.assert_text("");
     }
 }
+
+#[cfg(test)]
+mod subscription_endpoint {
+    use serde::Serialize;
+    use axum::http::StatusCode;
+
+    use crate::new_test_app;
+
+    #[derive(Serialize)]
+    struct SubscriberForm {
+        name: String,
+        email: String,
+    }
+
+    #[tokio::test]
+    async fn valid_subscription() {
+         let server = new_test_app().await;
+
+         let body = SubscriberForm {
+             name: "foo".to_string(),
+             email: "bar".to_string(),
+         };
+         let response = server
+            .post("/subscriptions")
+            .form::<SubscriberForm>(&body)
+            .await;
+
+        assert!(response.status_code().is_success());
+    }
+
+    #[tokio::test]
+    async fn invalid_subscription() {
+        let server = new_test_app().await;
+
+        let body = SubscriberForm {
+            name: "".to_string(),
+            email: "".to_string(),
+        };
+        let response = server
+            .post("/subscriptions")
+            .expect_failure()
+            .form::<SubscriberForm>(&body)
+            .await;
+
+        assert_eq!(response.status_code(), StatusCode::BAD_REQUEST);
+    }
+}
