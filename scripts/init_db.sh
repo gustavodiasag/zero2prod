ecbabb4484e294df1db96cd0f0b92cd86e74c2f3
Gustavo Dias <gustavodias.aguiar1@gmail.com>
2024-03-22 18:23:02 -0300

    feat: add migration command and confirmation message

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -39,7 +39,10 @@ until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q';
   sleep 1
 done
 
->&2 echo "Postgrese is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT}."
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated."
